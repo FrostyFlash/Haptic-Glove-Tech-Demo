43a8a288846a295561ac54b6ad03b47cd5df05d5
Samuel Breaux <samuel.breaux03@gmail.com>
2023-03-20 00:27:05 -0500

    Fixed sandbox camera conflict and added smooth turning into the player controller.

--- opengloves-force-feedback-unity-demo-main/Assets/Scripts/PlayerController.cs
+++ opengloves-force-feedback-unity-demo-main/Assets/Scripts/PlayerController.cs
@@ -9,13 +9,28 @@ using Valve.VR.InteractionSystem;
 public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
+    public SteamVR_Action_Vector2 input2;
     public float speed = 1;
+    public float turnSpeed = 45f;
+    private CharacterController characterController;
     // Start is called before the first frame update
-
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
     // Update is called once per frame
     void Update()
     {
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-        transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction,Vector3.up);
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up)-new Vector3(0,9.81f,0)*Time.deltaTime);
+        if (input2.axis.x > 0.2f || input2.axis.x < -0.2f)
+        {
+            // Calculate the angle to turn based on input2 axis
+            float turnAmount = input2.axis.x * turnSpeed;
+
+            // Smoothly rotate the player over time
+            // Pivot the player on their current position
+            transform.RotateAround(transform.position, Vector3.up, turnAmount * Time.deltaTime);
+        }
     }
 }
--- opengloves-force-feedback-unity-demo-main/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ opengloves-force-feedback-unity-demo-main/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -113,6 +113,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 SmoothTurn
+        {
+            get
+            {
+                return SteamVR_Actions.default_SmoothTurn;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
