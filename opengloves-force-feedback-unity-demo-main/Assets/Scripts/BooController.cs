6791a19e9a835b8f239cacc965a544b5c94a754d
Samuel Breaux <samuel.breaux03@gmail.com>
2023-03-19 22:22:20 -0500

    Added Boo Enemy

---
+++ opengloves-force-feedback-unity-demo-main/Assets/Scripts/BooController.cs
@@ -0,0 +1,45 @@
+
+using UnityEngine;
+
+public class BooController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float detectionRange = 10.0f;
+
+    private Transform player;
+    private bool playerIsVisible = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        // Check if the player is within the detection range
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        Debug.Log("Distance to player: " + distanceToPlayer);
+        if (distanceToPlayer <= detectionRange)
+        {
+            playerIsVisible = true;
+        }
+        else
+        {
+            playerIsVisible = false;
+        }
+
+        // Move towards the player if they're not visible
+        if (!playerIsVisible)
+        {
+            transform.LookAt(player);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Stop moving and turning when in contact with the player
+            moveSpeed = 0.0f;
+            transform.LookAt(transform.position);
+        }
+    }
+
+}
